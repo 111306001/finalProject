07ff0f93685ef33eb228d2794236cc2b955ce8ea
EdwardLyu <147014804+EdwardLyu@users.noreply.github.com>
2024-01-02 15:11:02 +0800

    Add files via upload

---
+++ final/src/TreeNode.java
@@ -0,0 +1,13 @@
+package fp;
+
+public class TreeNode {
+	 keyword data;
+	    TreeNode left;
+	    TreeNode right;
+
+	    public TreeNode(keyword data) {
+	        this.data = data;
+	        this.left = null;
+	        this.right = null;
+	    }
+}
