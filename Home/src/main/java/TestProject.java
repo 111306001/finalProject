e66abf38f21d3030d6cbb5e3c84201fb994a3fe5
111306001 <116822531+111306001@users.noreply.github.com>
2024-01-02 21:58:51 +0800

    push

--- Home/src/main/java/TestProject.java
+++ Home/src/main/java/TestProject.java
@@ -1,22 +1,21 @@
-
-
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.HashMap;
 import java.util.Map.Entry;
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.*;
 
 /**
  * Servlet implementation class TestProject
  */
 @WebServlet("/TestProject")
 public class TestProject extends HttpServlet {
+	
+	ArrayList <Keyword> lst;
 	private static final long serialVersionUID = 1L;
     /**
      * @see HttpServlet#HttpServlet()
@@ -34,28 +33,37 @@ public class TestProject extends HttpServlet {
 		response.setCharacterEncoding("UTF-8");
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html");
-		if(request.getParameter("keyword")== null) {
-			String requestUri = request.getRequestURI();
-			request.setAttribute("requestUri", requestUri);
-			request.getRequestDispatcher("Search.jsp").forward(request, response);
-			return;
-			
-		}
-		GoogleQuery google = new GoogleQuery(request.getParameter("keyword"));
-		HashMap<String, String> query = google.query();
 		
-		String[][] s = new String[query.size()][2];
-		request.setAttribute("query", s);
-		int num = 0;
-		for(Entry<String, String> entry : query.entrySet()) {
-		    String key = entry.getKey();
-		    String value = entry.getValue();
-		    s[num][0] = key;
-		    s[num][1] = value;
-		    num++;
+		for(int i = 0; i < request.getParameter("keyword").hashCode(); i++) {
+			String input = request.getParameter("keyword");
+			Keyword keyword = new Keyword(input, 0);
+			lst.add(keyword);
+			if(input == null) {
+				String requestUri = request.getRequestURI();
+				request.setAttribute("requestUri", requestUri);
+				request.getRequestDispatcher("Search.jsp").forward(request, response);
+				return;
+			}
 		}
-		request.getRequestDispatcher("googleitem.jsp")
-		 .forward(request, response); 
+		
+		String words = "";
+		for(int i = 0; i < lst.size(); i++) {
+			words += lst.get(i).name + " ";
+		}
+		
+		GoogleQuery google = new GoogleQuery(words);
+		HashMap<String, String> query = google.query();
+		String[][] s = new String[query.size()][2];
+			request.setAttribute("query", s);
+			int num = 0;
+			for(Entry<String, String> entry : query.entrySet()) {
+			    String key = entry.getKey();
+			    String value = entry.getValue();
+			    s[num][0] = key;
+			    s[num][1] = value;
+			    num++;
+			}
+		request.getRequestDispatcher("googleitem.jsp").forward(request, response); 
 	}
 
 	/**
