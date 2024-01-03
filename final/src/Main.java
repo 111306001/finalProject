c24c3c209b116d4c9fc6c3334355db8628993801
111306001 <116822531+111306001@users.noreply.github.com>
2024-01-03 11:06:15 +0800

    6

--- final/src/Main.java
+++ final/src/Main.java
@@ -1,4 +1,14 @@
 
-public class Main {
-
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Main{
+public static void Main(String[] args) {
+	ArrayList<String> list=new ArrayList<>();
+	Scanner sc =new Scanner(System.in);
+	while(sc.hasNext()) {
+		String keyword= sc.next();
+		list.add(keyword);
+		sc.close();
+	}
+}
 }
