d4605295b8ab206c17de5f34c118725c234eeb4e
EdwardLyu <147014804+EdwardLyu@users.noreply.github.com>
2024-01-08 23:50:33 +0800

    Update Web.java

--- Home/src/main/java/Web.java
+++ Home/src/main/java/Web.java
@@ -2,14 +2,18 @@
 public class Web {
 	public WebPage rootPage;
 	public WebTree tree;
-	
+	public KeywordList keywordList;
 	public Web(String url) {
 		// TODO Auto-generated method stub
 		rootPage = new WebPage(url);		
 		tree = new WebTree(rootPage);
+		this.keywordList = new KeywordList();
 	}
 	public void treeRootAddChild(String query, String word) {
 		tree.root.addChild(new WebNode(new WebPage(query)));
 	}
+	public void addKeywordToKeywordList(Keyword keyword) {
+        this.keywordList.add(keyword);
+    }
 
 }
